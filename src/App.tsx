3788114ac05837ca25e8a4aea39d34de990c10fe
Leonardo Vazquez <vazquez.leo.m@gmail.com>
2021-04-15 11:24:08 -0300

    extended theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { extendTheme } from "@chakra-ui/react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <ColorModeSwitcher justifySelf="flex-end" />
-  </ChakraProvider>
-);
+const theme = extendTheme({
+  fonts: { body: "Source Sans Pro", heading: "Raleway" },
+  colors: {
+    brand: {
+      100: "#000115",
+      500: "#62CBC7",
+      700: "#ED4C42",
+    },
+  },
+});
+
+export const App = () => <ChakraProvider theme={theme}></ChakraProvider>;
